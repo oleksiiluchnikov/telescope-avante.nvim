f5463ff57330fdb82b8cefcb020c27ea34ba9221
Oleksii Luchnikov <oleksiiluchnikov@gmail.com>
2025-02-07 00:12:22 +0200

    feat: README, and cleaner

--- README.md
+++ README.md
@@ -1,24 +1,55 @@
-# telescope-avante
+# telescope-avante.nvim
 
-A [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim) extension to switch providers for [avante.nvim](https://github.com/yetone/avante.nvim).
+A Telescope extension for [avante.nvim](https://github.com/olivercederborg/telescope-media-files.nvim) that allows you to switch avante providers.
+
+## Features
+
+- Switch avante providers using Telescope.
+- Elegant and mini picker UI.
 
 ## Installation
 
-Install using your favorite plugin manager:
+Install the extension with your package manager of choice:
+
+### [lazy.nvim](https://github.com/folke/lazy.nvim)
 
 ```lua
 {
-  'oleksiiluchnikov/telescope-avante.nvim',
-  dependencies = { 'nvim-telescope/telescope.nvim' },
+    'oleksiiluchnikov/telescope-avante.nvim',
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
+        'yetone/avante.nvim' }
 }
 ```
 
 ## Usage
 
 ```lua
-require('telescope').load_extension('telescope-avante')
+:Telescope avante
 ```
+
+You can also add a key mapping:
+
+```lua
+vim.keymap.set('n', '<leader>ap',
+    ':Telescope avante<CR>',
+    { desc = 'Switch Avante Provider' }
+)
+```
+
+## Configuration
+
+No configuration options are available.
+
+## Dependencies
+
+- [nvim-telescope/telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
+- [avante.nvim](https://github.com/yetone/avante.nvim)
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request.
 
 ## License
 
-[MIT](https://choosealicense.com/licenses/mit/)
+[MIT](LICENSE)
--- lua/telescope/_extensions/avante.lua
+++ lua/telescope/_extensions/avante.lua
@@ -1,42 +1,52 @@
-local has_telescope = pcall(require, "telescope")
+---@mod telescope-avante Telescope picker for Avante providers
+---@brief [[
+--- A telescope extension for switching between Avante providers
+--- with an elegant minimal UI.
+---@brief ]]
+
+-- Dependencies check
+local has_telescope, telescope = pcall(require, "telescope")
 if not has_telescope then
-	error("telescope-avante.nvim requires telescope.nvim - https://github.com/nvim-telescope/telescope.nvim")
+	error("telescope-avante.nvim requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)")
 end
+
+local has_avante, _ = pcall(require, "avante")
+if not has_avante then
+	error("telescope-avante.nvim requires avante.nvim (https://github.com/yetone/avante.nvim)")
+end
+
+-- Telescope modules
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local pickers = require("telescope.pickers")
 local sorters = require("telescope.sorters")
 local finders = require("telescope.finders")
 local themes = require("telescope.themes")
-local conf = require("telescope.config").values
 
-local has_telescope, telescope = pcall(require, "telescope")
-if not has_telescope then
-	error("This plugin requires nvim-telescope/telescope.nvim")
-end
+local THEME = {
+	width = 0.25, -- 25% of screen width
+	height = 0.35, -- 35% of screen height
+	winblend = 10, -- Slight transparency
+	border = true, -- Show border
+	previewer = false, -- No previewer needed
+	prompt_title = false,
+	results_title = false,
+}
 
 local M = {}
 
--- Get current avante provider
+-- Get current avante provider with fallback
+---@return string
 local function get_current_provider()
 	return vim.g.avante_provider or ""
 end
 
+---@param opts table|nil
 function M.avante(opts)
-	-- Merge user opts with dropdown theme
-	opts = vim.tbl_deep_extend(
-		"force",
-		themes.get_dropdown({
-			width = 0.3,
-			previewer = false,
-			prompt_title = false,
-			results_title = false,
-			winblend = 10,
-		}),
-		opts or {}
-	)
+	-- Merge user opts with our elegant theme
+	opts = vim.tbl_deep_extend("force", themes.get_dropdown(THEME), opts or {})
 
-	local current_provider = get_current_provider()
+	local current = get_current_provider()
 
 	pickers
 		.new(opts, {
@@ -45,7 +55,7 @@ function M.avante(opts)
 				entry_maker = function(provider)
 					return {
 						value = provider,
-						display = provider .. (provider == current_provider and " " or ""),
+						display = provider,
 						ordinal = provider,
 					}
 				end,
@@ -53,12 +63,12 @@ function M.avante(opts)
 			sorter = sorters.get_generic_fuzzy_sorter(),
 			attach_mappings = function(prompt_bufnr)
 				actions.select_default:replace(function()
-					actions.close(prompt_bufnr)
 					local selection = action_state.get_selected_entry()
-					if not selection then
-						return
+					actions.close(prompt_bufnr)
+
+					if selection and selection.value ~= current then
+						require("avante.api").switch_provider(selection.value)
 					end
-					require("avante.api").switch_provider(vim.trim(selection.value))
 				end)
 				return true
 			end,
@@ -66,7 +76,8 @@ function M.avante(opts)
 		:find()
 end
 
-return require("telescope").register_extension({
+-- Register telescope extension
+return telescope.register_extension({
 	exports = {
 		avante = M.avante,
 	},
